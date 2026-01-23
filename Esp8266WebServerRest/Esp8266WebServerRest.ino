5615606a49a3355d6ae9161ee43e985919e13410
Luigi Santagada <luigi.santagada@live.com>
2026-01-23 14:07:03 +0100

    risistemate librerie e codice per anni passati.

--- Esp8266WebServerRest/Esp8266WebServerRest.ino
+++ Esp8266WebServerRest/Esp8266WebServerRest.ino
@@ -1,348 +1,185 @@
+// filename: Esp8266WebServerRest.ino
+
 #define FIXERASECONFIG
 
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <DNSServer.h>
+
+#include <WiFiManager.h>      // https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>      // ArduinoJson 7.x
+
 #include <stdio.h>
 
-#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
-
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-
-#include <ESP8266WebServer.h>
-
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic      
-
-#include <ArduinoJson.h>
-
 #include "index.h"
-
 #include "index2.h"
-
 
 #define HTTP_REST_PORT 80
 
-#define WIFI_RETRY_DELAY 500
-
-#define MAX_WIFI_INIT_RETRY 50
-
 WiFiManager wifiManager;
 
-
-//
-//const char* wifi_ssid = "HUAWEI Mate s_2GEXT";
-//
-//const char* wifi_passwd = "xxxxxxxxxxx";
-
 struct Led {
-
-	byte id;
-
-	byte gpio;
-
-	byte status;
-
+    byte id;
+    byte gpio;
+    byte status;
 } led_resource;
 
 ESP8266WebServer http_rest_server(HTTP_REST_PORT);
 
-void init_led_resource()
-
-{
-
-	led_resource.id = 0;
-
-	led_resource.gpio = 0;
-
-	led_resource.status = LOW;
-
+void init_led_resource() {
+    led_resource.id = 0;
+    led_resource.gpio = 0;
+    led_resource.status = LOW;
 }
 
-int init_wifi() {
-	WiFi.hostname("LsgOpener");
-	wifiManager.autoConnect("LGS-HOMECONTROLLER", "password");
-
+bool init_wifi() {
+    WiFi.hostname("LsgOpener");
+    // true se connesso
+    return wifiManager.autoConnect("LGS-HOMECONTROLLER", "password");
 }
 
 void get_leds() {
+    if (led_resource.id == 0) {
+        http_rest_server.send(204);
+        return;
+    }
+    JsonDocument doc;
+    doc["id"] = led_resource.id;
+    doc["gpio"] = led_resource.gpio;
+    doc["status"] = led_resource.status;
 
-	StaticJsonBuffer<200> jsonBuffer;
+    char out[200];
+    size_t n = serializeJsonPretty(doc, out, sizeof(out));
+    if (n == 0) {
+        http_rest_server.send(500, "text/plain", "JSON serialize error");
+        return;
+    }
 
-	JsonObject& jsonObj = jsonBuffer.createObject();
-
-	char JSONmessageBuffer[200];
-
-	if (led_resource.id == 0)
-
-		http_rest_server.send(204);
-
-	else {
-
-		jsonObj["id"] = led_resource.id;
-
-		jsonObj["gpio"] = led_resource.gpio;
-
-		jsonObj["status"] = led_resource.status;
-
-		jsonObj.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-
-		http_rest_server.send(200, "application/json", JSONmessageBuffer);
-
-	}
-
+    http_rest_server.send(200, "application/json", out);
 }
 
-void json_to_resource(JsonObject& jsonBody) {
+void json_to_resource(JsonObject obj) {
+    int id = obj["id"] | 0;
+    int gpio = obj["gpio"] | 0;
+    int status = obj["status"] | 0;
 
-	int id, gpio, status;
+    Serial.println(id);
+    Serial.println(gpio);
+    Serial.println(status);
 
-	id = jsonBody["id"];
-
-	gpio = jsonBody["gpio"];
-
-	status = jsonBody["status"];
-
-	Serial.println(id);
-
-	Serial.println(gpio);
-
-	Serial.println(status);
-
-	led_resource.id = id;
-
-	led_resource.gpio = gpio;
-
-	led_resource.status = status;
-
+    led_resource.id = (byte)id;
+    led_resource.gpio = (byte)gpio;
+    led_resource.status = (byte)status;
 }
 
 void post_put_leds() {
+    String post_body = http_rest_server.arg("plain");
+    Serial.println(post_body);
 
-	StaticJsonBuffer<500> jsonBuffer;
+    JsonDocument doc;
+    DeserializationError err = deserializeJson(doc, post_body);
+    if (err) {
+        Serial.print("JSON parse error: ");
+        Serial.println(err.c_str());
+        http_rest_server.send(400, "text/plain", "Bad JSON");
+        return;
+    }
 
-	String post_body = http_rest_server.arg("plain");
+    JsonObject jsonBody = doc.as<JsonObject>();
 
-	Serial.println(post_body);
+    Serial.print("HTTP Method: ");
+    Serial.println(http_rest_server.method());
 
-	JsonObject& jsonBody = jsonBuffer.parseObject(http_rest_server.arg("plain"));
+    if (http_rest_server.method() == HTTP_POST) {
 
-	Serial.print("HTTP Method: ");
+        int id = jsonBody["id"] | 0;
+        if (id != 0 && (byte)id != led_resource.id) {
 
-	Serial.println(http_rest_server.method());
+            json_to_resource(jsonBody);
 
-	if (!jsonBody.success()) {
+            http_rest_server.sendHeader("Location", "/leds/" + String(led_resource.id));
+            http_rest_server.send(201);
 
-		Serial.println("error in parsin json body");
+            pinMode(led_resource.gpio, OUTPUT);
 
-		http_rest_server.send(400);
+        }
+        else if (id == 0) {
+            http_rest_server.send(404);
+        }
+        else {
+            http_rest_server.send(409);
+        }
 
-	}
+    }
+    else if (http_rest_server.method() == HTTP_PUT) {
 
-	else {
+        int id = jsonBody["id"] | 0;
+        if ((byte)id == led_resource.id) {
 
-		if (http_rest_server.method() == HTTP_POST) {
+            json_to_resource(jsonBody);
 
-			if ((jsonBody["id"] != 0) && (jsonBody["id"] != led_resource.id)) {
+            http_rest_server.sendHeader("Location", "/leds/" + String(led_resource.id));
+            http_rest_server.send(200);
 
-				json_to_resource(jsonBody);
+            digitalWrite(led_resource.gpio, led_resource.status);
 
-				http_rest_server.sendHeader("Location", "/leds/" + String(led_resource.id));
-
-				http_rest_server.send(201);
-
-				pinMode(led_resource.gpio, OUTPUT);
-
-			}
-
-			else if (jsonBody["id"] == 0)
-
-				http_rest_server.send(404);
-
-			else if (jsonBody["id"] == led_resource.id)
-
-				http_rest_server.send(409);
-
-		}
-
-		else if (http_rest_server.method() == HTTP_PUT) {
-
-			if (jsonBody["id"] == led_resource.id) {
-
-				json_to_resource(jsonBody);
-
-				http_rest_server.sendHeader("Location", "/leds/" + String(led_resource.id));
-
-				http_rest_server.send(200);
-
-				digitalWrite(led_resource.gpio, led_resource.status);
-
-			}
-
-			else
-
-				http_rest_server.send(404);
-
-		}
-
-	}
-
+        }
+        else {
+            http_rest_server.send(404);
+        }
+    }
 }
 
 void resetwifi() {
-	http_rest_server.send(200, "text/html", "System reset");
+    http_rest_server.send(200, "text/html", "System reset");
+    delay(2000);
 
-	delay(5000);
+    wifiManager.resetSettings();
 
-	wifiManager.resetSettings();
+    // Se vuoi riavvio dopo reset:
+    // ESP.restart();
 }
 
 void config_rest_server_routing() {
+    http_rest_server.on("/menu", HTTP_GET, []() {
+        String s = MAIN_page2;
+        http_rest_server.send(200, "text/html", s);
+        Serial.write("Dati da inviare al microcontrollore");
+        });
 
-	Serial.println("entrato");
-	
-	http_rest_server.on("/menu", HTTP_GET, []() {
+    http_rest_server.on("/reset", HTTP_GET, []() {
+        resetwifi();
+        });
 
-		Serial.println("entrato");
-
-		String s = MAIN_page2; //Read HTML contents
-
-		http_rest_server.send(200, "text/html", s);
-
-		Serial.write("Dati da inviare al microcontrollore");
-		//for (int i = 0; i < 5; i++)
-		//{
-			//Serial.println("Apro il portone");
-		/*	digitalWrite(2, HIGH);
-			delay(1000);
-			digitalWrite(2, LOW);
-			delay(1000);*/
-		//}
-	});
-
-	http_rest_server.on("/reset", HTTP_GET, []() {
-
-		resetwifi();
-
-		return;
-
-		http_rest_server.send(200, "text/html",
-			"host resetted");
-
-		
-
-		delay(5000);
-
-		WiFi.hostname("LsgOpener");
-		//////Serial.println("Connecting to WiFi AP..........");
-		wifiManager.resetSettings();
-		ESP.reset();
-
-	
-		wifiManager.autoConnect("LGS-HOMECONTROLLER", "password");
-
-		config_rest_server_routing();
-
-		http_rest_server.begin();
-
-
-
-	});
-
-	http_rest_server.on("/leds", HTTP_GET, get_leds);
-
-	http_rest_server.on("/leds", HTTP_POST, post_put_leds);
-
-	http_rest_server.on("/leds", HTTP_PUT, post_put_leds);
-
+    http_rest_server.on("/leds", HTTP_GET, get_leds);
+    http_rest_server.on("/leds", HTTP_POST, post_put_leds);
+    http_rest_server.on("/leds", HTTP_PUT, post_put_leds);
 }
 
-void setup(void) {
+void setup() {
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println("Start");
 
-	Serial.begin(9600);
+    init_led_resource();
 
+    if (init_wifi()) {
+        Serial.print("Connected --- IP: ");
+        Serial.println(WiFi.localIP());
+    }
+    else {
+        Serial.println("Error connecting (autoConnect failed)");
+    }
 
+    config_rest_server_routing();
+    http_rest_server.begin();
+    Serial.println("HTTP REST Server Started");
 
-	Serial.print("Start");
-
-
-	init_led_resource();
-
-	if (init_wifi() == WL_CONNECTED) {
-
-		Serial.print("Connetted to ");
-
-		//Serial.print(wifi_ssid);
-
-		Serial.print("--- IP: ");
-
-		Serial.println(WiFi.localIP());
-
-	}
-
-	else {
-
-		Serial.print("Error connecting to: ");
-
-		//Serial.println(wifi_ssid);
-
-	}
-	
-	config_rest_server_routing();
-
-	http_rest_server.begin();
-
-	Serial.println("HTTP REST Server Started");
-
-	pinMode(2, OUTPUT);
+    pinMode(2, OUTPUT);
 }
 
-void loop(void) {
-	//HTTPClient http;    //Declare object of class HTTPClient
-	//http.begin("http://192.168.0.182:901/WcfServiceEsp8266/WcfServiceBase.svc/GetDataC");      //Specify request destination
-	//http.addHeader("Content-Type" , "application/json");  //Specify content-type header
-	//int httpCode = http.POST("{\"email\" : \"tin@it\",\"name\" : \"luigis\",\"username\" : \"hp9000\"}");   //Send the request
-	//String payload = http.getString();                  //Get the response payload
-	//Serial.println(httpCode);   //Print HTTP return code
-	//Serial.println(payload);    //Print request response payload
-	//http.end();  //Close connection
-
-
-
-
-	////Parte che chiama un servizio rest service
-
-	//HTTPClient http; //Object of class HTTPClient
-	////http.begin("http://jsonplaceholder.typicode.com/users/1");
-	//
-	//http.begin("http://192.168.0.182:901/WcfServiceEsp8266/WcfServiceBase.svc/GetData");
-
-	////delay(2000);
-
-	//int httpCode = http.GET();
-
-	////Serial.println("Client : "); Serial.println(httpCode);
-
-	//if (httpCode > 0)
-	//{
-	//	const size_t bufferSize = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(2);// +JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
-	//	DynamicJsonBuffer jsonBuffer(bufferSize);
-	//	JsonObject& root = jsonBuffer.parseObject(http.getString());
-
-	//	int id = root["id"];
-	//	const char* name = root["name"];
-	//	const char* username = root["username"];
-	//	const char* email = root["email"];
-
-	//	Serial.print("Name:");
-	//	Serial.println(name);
-	//	Serial.print("Username:");
-	//	Serial.println(username);
-	//	Serial.print("Email:");
-	//	Serial.println(email);
-	//}
-	//http.end(); //Close connection
-
-
-	//delay(10000);
-
-	http_rest_server.handleClient();
+void loop() {
+    http_rest_server.handleClient();
 }
-
